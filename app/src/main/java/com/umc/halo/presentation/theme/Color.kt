2f4e345ebef7e9e7d509293d325599118f5c3885
LLUPINUS <doyeop2002@gmail.com>
2026-07-04 20:11:01 +0900

    feat: 디자인 시스템 색상 정의 추가

--- app/src/main/java/com/umc/halo/presentation/theme/Color.kt
+++ app/src/main/java/com/umc/halo/presentation/theme/Color.kt
@@ -2,10 +2,32 @@ package com.umc.halo.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//---- Primary ----
+val Primary600 = Color(0XFFA84C00)
+val Primary500 = Color(0XFFFF7B10)
+val Primary400 = Color(0XFFFF9742)
+val Primary300 = Color(0XFFFFB375)
+val Primary200 = Color(0XFFFFCFA8)
+val Primary100 = Color(0XFFFFEBDB)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+//---- Black & White ----
+val Black = Color(0XFF000000)
+val White = Color(0XFFffffff)
+
+//---- Gray ----
+val Gray900 = Color(0XFF1A1A1A)
+val Gray800 = Color(0XFF2B2B2B)
+val Gray700 = Color(0XFF404040)
+val Gray600 = Color(0XFF595959)
+val Gray500 = Color(0XFF737373)
+val Gray400 = Color(0XFF8C8C8C)
+val Gray300 = Color(0XFFB3B3B3)
+val Gray200 = Color(0XFFD9D9D9)
+val Gray100 = Color(0XFFF5F5F5)
+
+//---- System ----
+val Success = Color(0XFF34C759)
+val Error   = Color(0XFFED433A)
+
+//---- Sub ----
+val Sub500  = Color(0XFF00c6cc)
