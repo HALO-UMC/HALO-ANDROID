6749bb195e5fa65aa9b196c008f8ad4b3d24b3f0
LLUPINUS <doyeop2002@gmail.com>
2026-07-07 20:26:17 +0900

    feat: 디자인 시스템(색상) 업데이트

--- app/src/main/java/com/umc/halo/presentation/theme/Color.kt
+++ app/src/main/java/com/umc/halo/presentation/theme/Color.kt
@@ -3,12 +3,14 @@ package com.umc.halo.presentation.theme
 import androidx.compose.ui.graphics.Color
 
 //---- Primary ----
-val Primary600 = Color(0XFFA84C00)
+val Primary600 = Color(0XFFFF6B0D)
 val Primary500 = Color(0XFFFF7B10)
 val Primary400 = Color(0XFFFF9742)
 val Primary300 = Color(0XFFFFB375)
 val Primary200 = Color(0XFFFFCFA8)
 val Primary100 = Color(0XFFFFEBDB)
+val Primary50  = Color(0XFFFFF3E8)
+val Primary30  = Color(0XFFFFFAF7)
 
 //---- Black & White ----
 val Black = Color(0XFF000000)
@@ -23,11 +25,11 @@ val Gray500 = Color(0XFF737373)
 val Gray400 = Color(0XFF8C8C8C)
 val Gray300 = Color(0XFFB3B3B3)
 val Gray200 = Color(0XFFD9D9D9)
-val Gray100 = Color(0XFFF5F5F5)
+val Gray100 = Color(0XFFEEEEEE)
+val Gray50  = Color(0XFFF7F7F7)
+val Gray30  = Color(0XFFFAFAFA)
 
 //---- System ----
 val Success = Color(0XFF34C759)
 val Error   = Color(0XFFED433A)
 
-//---- Sub ----
-val Sub500  = Color(0XFF00c6cc)
