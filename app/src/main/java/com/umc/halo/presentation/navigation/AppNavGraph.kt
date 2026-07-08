0810fb84a67f785b77fe75edee307e6eb9c59e9e
Nick9417 <kimjaehwan0920@gmail.com>
2026-07-08 20:53:24 +0900

    feat: BottomNav divider 추가

--- app/src/main/java/com/umc/halo/presentation/navigation/AppNavGraph.kt
+++ app/src/main/java/com/umc/halo/presentation/navigation/AppNavGraph.kt
@@ -1,5 +1,10 @@
 package com.umc.halo.presentation.navigation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -11,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -33,10 +39,25 @@ fun AppNavGraph(
     Scaffold(
         bottomBar = {
             if (currentRoute in bottomNavRoutes) {
-                BottomBar(
-                    navController = navController,
-                    currentRoute = currentRoute
-                )
+                Column {
+                    Box(modifier = Modifier
+                        .fillMaxWidth()
+                        .height(10.dp)
+                        .background(
+                            Brush.verticalGradient(
+                                colors = listOf(
+                                    Color.Transparent,
+                                    Color(0xFF858585).copy(alpha = 0.2f)
+                                )
+                            )
+                        )
+                    )
+
+                    BottomBar(
+                        navController = navController,
+                        currentRoute = currentRoute
+                    )
+                }
             }
         }
     ) { padding ->
@@ -44,7 +65,7 @@ fun AppNavGraph(
         //--Route와 일치하는 화면 출력
         NavHost(
             navController = navController,
-            startDestination = Routes.SPLASH,
+            startDestination = Routes.HOME,
             modifier = Modifier.padding(padding)
         ) {
             composable(Routes.SPLASH) {
