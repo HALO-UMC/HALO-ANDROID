4ab20616e2e862a1f90eb526d9fbf3cf0c9ec5bc
Nick9417 <kimjaehwan0920@gmail.com>
2026-07-09 18:35:57 +0900

    feat: 상단 배너 구현

--- app/src/main/java/com/umc/halo/presentation/component/HaloScaffold.kt
+++ app/src/main/java/com/umc/halo/presentation/component/HaloScaffold.kt
@@ -15,7 +15,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.umc.halo.presentation.home.HomeTopBar
 import com.umc.halo.presentation.navigation.BottomNavItem
+import com.umc.halo.presentation.navigation.Routes
 
 @Composable
 fun HaloScaffold(
@@ -30,7 +32,7 @@ fun HaloScaffold(
     Scaffold(
         topBar = {
             when(currentRoute) {
-                //Routes.HOME -> HomeTopBar()
+                Routes.HOME -> HomeTopBar()
                 //Routes.MYPAGE -> MyPageTopBar()
                 //Routes.CALENDAR -> CalendarTopBar()
                 //Routes.THEME_BOX -> ThemeBoxTopBar()
--- app/src/main/java/com/umc/halo/presentation/home/HomeTopBar.kt
+++ app/src/main/java/com/umc/halo/presentation/home/HomeTopBar.kt
@@ -1,4 +1,12 @@
 package com.umc.halo.presentation.home
 
-class HomeTopBar {
+import androidx.compose.runtime.Composable
+import com.umc.halo.presentation.component.HaloTopBar
+
+@Composable
+fun HomeTopBar() {
+    HaloTopBar(
+        title = "HALO",
+        showLeftIcon = false
+    )
 }
