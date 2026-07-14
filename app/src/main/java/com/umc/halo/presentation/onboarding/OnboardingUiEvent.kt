2031061b738b38fe6a6faf9f8a584a6fb27f8b8c
chrry03 <ocr925@dgu.ac.kr>
2026-07-14 19:48:53 +0900

    feat: onboardinguievent에 생년월일 선택형필드로 바꿈

--- app/src/main/java/com/umc/halo/presentation/onboarding/OnboardingUiEvent.kt
+++ app/src/main/java/com/umc/halo/presentation/onboarding/OnboardingUiEvent.kt
@@ -4,9 +4,10 @@ sealed interface OnboardingUiEvent {
     data class NameChanged(val name: String) : OnboardingUiEvent
 
     data class GenderSelected(val gender: Gender) : OnboardingUiEvent
-    data class BirthYearChanged(val year: String) : OnboardingUiEvent
-    data class BirthMonthChanged(val month: String) : OnboardingUiEvent
-    data class BirthDayChanged(val day: String) : OnboardingUiEvent
+
+    data class BirthYearSelected(val year: Int) : OnboardingUiEvent
+    data class BirthMonthSelected(val month: Int) : OnboardingUiEvent
+    data class BirthDaySelected(val day: Int) : OnboardingUiEvent
 
     data class ParentPersonalityClicked(val personality: String) : OnboardingUiEvent
     data class RelationshipClicked(val relationship: String) : OnboardingUiEvent
