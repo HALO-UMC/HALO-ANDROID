04bf82e419a2e937a6d9fa98c4b40053a9299947
Nick9417 <kimjaehwan0920@gmail.com>
2026-07-18 16:36:43 +0900

    feat: ThemeBox 기본 설정

---
+++ app/src/main/java/com/umc/halo/presentation/themebox/ThemeBoxScreen.kt
@@ -0,0 +1,15 @@
+package com.umc.halo.presentation.themebox
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ThemeBoxScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ThemeBoxPreview() {
+    ThemeBoxScreen()
+}
