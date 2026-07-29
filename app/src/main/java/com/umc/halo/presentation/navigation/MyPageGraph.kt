c008d27d4cee7d9a5bc550fef483d1e1548547ab
Nick9417 <kimjaehwan0920@gmail.com>
2026-07-29 18:19:06 +0900

    feat: MypageGraph

--- app/src/main/java/com/umc/halo/presentation/navigation/MyPageGraph.kt
+++ app/src/main/java/com/umc/halo/presentation/navigation/MyPageGraph.kt
@@ -1,2 +1,155 @@
 package com.umc.halo.presentation.navigation
 
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.umc.halo.presentation.mypage.AccountInfoRoute
+import com.umc.halo.presentation.mypage.AnniversaryRoute
+import com.umc.halo.presentation.mypage.NotificationSettingsRoute
+import com.umc.halo.presentation.mypage.SystemSettingsRoute
+import com.umc.halo.presentation.mypage.WithdrawRoute
+import com.umc.halo.presentation.mypage.screen.AccountManagementScreen
+import com.umc.halo.presentation.mypage.screen.MyPageScreen
+import com.umc.halo.presentation.mypage.screen.OpenLicenseScreen
+import com.umc.halo.presentation.mypage.screen.PrivacyPolicyScreen
+import com.umc.halo.presentation.mypage.screen.RelationshipInfoScreen
+import com.umc.halo.presentation.mypage.screen.TermsScreen
+
+fun NavGraphBuilder.mypageGraph(
+    navController: NavController
+) {
+    navigation(
+        route = Graphs.MYPAGE,
+        startDestination = Routes.MYPAGE
+    ) {
+        composable(Routes.MYPAGE) {
+            MyPageScreen(
+                onNavigateToRelationshipInfo = {
+                    navController.navigate(Routes.MYPAGE_RELATIONSHIP_INFO) {
+                        launchSingleTop = true
+                    }
+                },
+                onNavigateToAnniversary = {
+                    navController.navigate(Routes.MYPAGE_ANNIVERSARY) {
+                        launchSingleTop = true
+                    }
+                },
+                onNavigateToSystemSettings = {
+                    navController.navigate(Routes.MYPAGE_SYSTEM_SETTINGS) {
+                        launchSingleTop = true
+                    }
+                },
+                onNavigateToNotificationSettings = {
+                    navController.navigate(Routes.MYPAGE_NOTIFICATION_SETTINGS) {
+                        launchSingleTop = true
+                    }
+                },
+                onNavigateToAccountManagement = {
+                    navController.navigate(Routes.MYPAGE_ACCOUNT_MANAGEMENT) {
+                        launchSingleTop = true
+                    }
+                }
+            )
+        }
+
+        composable(Routes.MYPAGE_RELATIONSHIP_INFO) {
+            RelationshipInfoScreen(onBack = navController::popBackStack)
+        }
+
+        composable(Routes.MYPAGE_ANNIVERSARY) {
+            AnniversaryRoute(onBack = navController::popBackStack)
+        }
+
+        composable(Routes.MYPAGE_SYSTEM_SETTINGS) {
+            SystemSettingsRoute(onBack = navController::popBackStack)
+        }
+
+        composable(Routes.MYPAGE_NOTIFICATION_SETTINGS) {
+            NotificationSettingsRoute(onBack = navController::popBackStack)
+        }
+
+        composable(Routes.MYPAGE_ACCOUNT_MANAGEMENT) {
+            AccountManagementScreen(
+                onBack = navController::popBackStack,
+                onNavigateToAccountInfo = {
+                    navController.navigate(Routes.MYPAGE_ACCOUNT_INFO) {
+                        launchSingleTop = true
+                    }
+                },
+                onNavigateToOpenLicense = {
+                    navController.navigate(Routes.MYPAGE_OPEN_LICENSE) {
+                        launchSingleTop = true
+                    }
+                },
+                onNavigateToTerms = {
+                    navController.navigate(Routes.MYPAGE_TERMS) {
+                        launchSingleTop = true
+                    }
+                }
+            )
+        }
+
+        composable(Routes.MYPAGE_ACCOUNT_INFO) {
+            AccountInfoRoute(
+                onBack = navController::popBackStack,
+                onNavigateToWithdraw = {
+                    navController.navigate(Routes.MYPAGE_WITHDRAW) {
+                        launchSingleTop = true
+                    }
+                },
+                onNavigateToLogin = {
+                    navController.navigate(Routes.LOGIN) {
+                        popUpTo(navController.graph.id) {
+                            inclusive = true
+                        }
+                        launchSingleTop = true
+                    }
+                }
+            )
+        }
+
+        composable(Routes.MYPAGE_WITHDRAW) {
+            WithdrawRoute(
+                onBack = navController::popBackStack,
+                onNavigateToLogin = {
+                    navController.navigate(Routes.LOGIN) {
+                        popUpTo(navController.graph.id) {
+                            inclusive = true
+                        }
+                        launchSingleTop = true
+                    }
+                }
+            )
+        }
+
+        composable(Routes.MYPAGE_TERMS) {
+            TermsScreen(onBack = navController::popBackStack)
+        }
+
+        composable(Routes.MYPAGE_OPEN_LICENSE) {
+            OpenLicenseScreen(
+                onBack = navController::popBackStack,
+                onNavigateToAccountInfo = {
+                    navController.navigate(Routes.MYPAGE_ACCOUNT_INFO) {
+                        launchSingleTop = true
+                    }
+                },
+                onNavigateToPrivacyPolicy = {
+                    navController.navigate(Routes.MYPAGE_PRIVACY_POLICY) {
+                        launchSingleTop = true
+                    }
+                },
+                onNavigateToTerms = {
+                    navController.navigate(Routes.MYPAGE_TERMS) {
+                        launchSingleTop = true
+                    }
+                }
+            )
+        }
+
+        composable(Routes.MYPAGE_PRIVACY_POLICY) {
+            PrivacyPolicyScreen(onBack = navController::popBackStack)
+        }
+    }
+}
