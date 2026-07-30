7ec4038c41a29d8d107d29062236685f66dae7c9
Nick9417 <kimjaehwan0920@gmail.com>
2026-07-31 02:53:26 +0900

    fix: Home navigation

--- app/src/main/java/com/umc/halo/presentation/navigation/main_graph/HomeGraph.kt
+++ app/src/main/java/com/umc/halo/presentation/navigation/main_graph/HomeGraph.kt
@@ -2,11 +2,17 @@ package com.umc.halo.presentation.navigation.main_graph
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import androidx.navigation.navigation
 import com.umc.halo.presentation.home.HomeRoute
 import com.umc.halo.presentation.navigation.Graphs
 import com.umc.halo.presentation.navigation.Routes
+import com.umc.halo.presentation.storybook.chapter.ChapterProgressRoute
+import com.umc.halo.presentation.storybook.chapter.ChapterResultRoute
+import com.umc.halo.presentation.storybook.detail.StoryBookDetailRoute
+import com.umc.halo.presentation.themebox.show_theme.ShowThemeRoute
 
 fun NavGraphBuilder.homeGraph(
     navController: NavController
@@ -26,19 +32,100 @@ fun NavGraphBuilder.homeGraph(
         }
 
         composable(Routes.STORYBOOK_DETAIL) {
-
+            StoryBookDetailRoute(
+                onNavigateToChapterResult = { storybookId, chapterId ->
+                    navController.navigate(Routes.chapterResult(storybookId,chapterId)) {
+                        launchSingleTop = true
+                    }
+                },
+                onNavigateToChapterProgress = { storybookId, chapterId ->
+                    navController.navigate(Routes.chapterProgress(storybookId,chapterId)) {
+                        launchSingleTop = true
+                    }
+                },
+                onNavigateToShowTheme = { storybookId ->
+                    navController.navigate(Routes.showTheme(storybookId)) {
+                        launchSingleTop = true
+                    }
+                }
+            )
         }
 
-        composable(Routes.CHAPTER_PROGRESS) {
+        composable(
+            route = Routes.CHAPTER_PROGRESS,
+            arguments = listOf(
+                navArgument("storybookId") {
+                    type = NavType.LongType
+                },
+                navArgument("chapterId") {
+                    type = NavType.LongType
+                }
+            )
+        ) { backStackEntry ->
+            val storybookId = backStackEntry.arguments
+                ?.getLong("storybookId")
+                ?: return@composable
 
+            val chapterId = backStackEntry.arguments
+                ?.getLong("chapterId")
+                ?: return@composable
+
+            ChapterProgressRoute(
+                storybookId = storybookId,
+                chapterId = chapterId,
+                onNavigateBack = {
+                    navController.popBackStack()
+                },
+                onNavigateToResult = { resultStorybookId, resultChapterId ->
+                    navController.navigate(
+                        Routes.chapterResult(
+                            storybookId = resultStorybookId,
+                            chapterId = resultChapterId
+                        )
+                    )
+                }
+            )
         }
 
-        composable(Routes.CHAPTER_RESULT) {
+        composable(
+            route = Routes.CHAPTER_RESULT,
+            arguments = listOf(
+                navArgument("storybookId") {
+                    type = NavType.LongType
+                },
+                navArgument("chapterId") {
+                    type = NavType.LongType
+                }
+            )
+        ) { backStackEntry ->
+            val storybookId = backStackEntry.arguments
+                ?.getLong("storybookId")
+                ?: return@composable
 
+            val chapterId = backStackEntry.arguments
+                ?.getLong("chapterId")
+                ?: return@composable
+
+            ChapterResultRoute(
+                storybookId = storybookId,
+                chapterId = chapterId,
+                onNavigateBack = {
+                    navController.navigate(Routes.storybookDetail(storybookId)) {
+                        popUpTo(Routes.storybookDetail(storybookId)) {
+                            inclusive = false
+                        }
+                        launchSingleTop = true
+                    }
+                }
+            )
         }
 
-        composable(Routes.THEME_BOX) {
-
+        composable(Routes.SHOW_THEME) {
+            ShowThemeRoute(
+                onNavigateBack = {
+                    navController.popBackStack()
+                }
+            )
         }
     }
 }
