3066f4aa67f6baa2b6654fa603c480b76cfa9673
Nick9417 <kimjaehwan0920@gmail.com>
2026-08-02 20:19:12 +0900

    fix: graph 네비게이션 수정

--- app/src/main/java/com/umc/halo/presentation/navigation/main_graph/StorybookGraph.kt
+++ app/src/main/java/com/umc/halo/presentation/navigation/main_graph/StorybookGraph.kt
@@ -42,7 +42,14 @@ fun NavGraphBuilder.storybookGraph(
             )
         }
 
-        composable(Routes.STORYBOOK_DETAIL) {
+        composable(
+            route = Routes.STORYBOOK_DETAIL,
+            arguments = listOf(
+                navArgument("storybookId") {
+                    type = NavType.LongType
+                }
+            )
+        ) {
             StoryBookDetailRoute(
                 onNavigateToChapterResult = { storybookId, chapterId ->
                     navController.navigate(Routes.chapterResult(storybookId,chapterId)) {
